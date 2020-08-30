9121246e8e6f52ef515069404754da47de0d86a8
Ka Lun Yeung <kalunyeung6@gmail.com>
2020-08-30 16:30:24 -0700

    Added a graph that shows daily net worth

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 function extractDates(jsonData)
 {
   var intendedData = [];
+  var dates = [];
+  var tradeData=[];
   var beginningDate = new Date(document.getElementById("BeginDate").value ); 
   var endingDate = new Date(document.getElementById("EndDate").value);
 
@@ -9,10 +11,12 @@ function extractDates(jsonData)
   $.each( jsonData, function(index,value){
     var currentDate = new Date(index);
     if (beginningDate <= currentDate && currentDate <= endingDate){
-      intendedData.push(value);
+      tradeData.push(value);
+      dates.push(index);
     }
 })
   
+  intendedData = [dates,tradeData]
   return intendedData;
   
 }
@@ -27,47 +31,86 @@ $.getJSON(url, function(data) {
   var RawData = data["Time Series (Daily)"]; 
   
   var AllData = extractDates(RawData);
-  if (AllData.length <1)
+  var tradingDate = AllData[0].reverse();
+  var tradingInformation =AllData[1];
+
+  
+  var dailyNetworth = [];
+
+  if (tradingDate <1)
   {
     
-    document.getElementById("demo").innerHTML = "Looks like that ";
+    document.getElementById("demo").innerHTML = "Looks like that there was no trading day in th specified range ";
     return;
   }
-
-
-
-  var begin = (AllData[AllData.length-1]["1. open"]); 
-  var end = (AllData[0]["4. close"]); 
+  
+  var begin = (tradingInformation[tradingInformation.length-1]["1. open"]); 
+  
   var stockAmount = document.getElementById("AmountEntered").value/parseFloat(begin); 
   
+  for (var i = tradingInformation.length-1; i >= 0; i--) {
 
-  for (var i = 0; i < AllData.length; i++) {
-    if (AllData[i]["8. split coefficient"] != "1.0000")
+    if (tradingInformation[i]["8. split coefficient"] != "1.0000")
     {
-      stockAmount = stockAmount *(AllData[i]["8. split coefficient"]);
+      
+      stockAmount = stockAmount *(tradingInformation[i]["8. split coefficient"]);
     }
-    
-}
-  
-
-  
-  
-  
-  
-  for (var i = 0; i < AllData.length; i++) {
-    if (AllData[i]["7. dividend amount"] != "0.0000")
+    if (tradingInformation[i]["7. dividend amount"] != "0.0000")
     {
-        var dividend = parseFloat(AllData[i]["7. dividend amount"]);
-        var openPrice = parseFloat(AllData[i]["1. open"]);
+        var dividend = parseFloat(tradingInformation[i]["7. dividend amount"]);
+        var openPrice = parseFloat(tradingInformation[i]["1. open"]);
         var reinvestment = dividend/openPrice;
+        console.log(dividend);
         stockAmount = stockAmount + reinvestment;
     }
-    
+
+
+    var currentNetWorth = stockAmount * tradingInformation[i]["4. close"]; 
+    dailyNetworth.push(currentNetWorth);
+
+  }
+  console.log(tradingDate);
+  console.log(dailyNetworth);
+  
+  var endTotal = dailyNetworth[dailyNetworth.length-1];
+  showGraph(tradingDate,   dailyNetworth, document.getElementById("stockSymbol").value);
+  document.getElementById("demo").innerHTML = endTotal;
+
+
+
+
+
+  }) //jquery ending
+} //function ending
+
+
+
+
+
+function showGraph(x_axis,y_axis,symbol){
+  var ctx = document.getElementById('myChart').getContext("2d");
+  ctx.canvas.width = 0;
+  ctx.canvas.height = 0;
+
+  var chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'line',
+
+      // The data for our dataset
+      data: {
+          labels: x_axis,
+          datasets: [{
+              label: symbol,
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: y_axis,
+              fill:false,
+          }]
+      },
+
+      // Configuration options go here
+      options: {}
+  });
+
 }
-  
-  
- var total = (parseFloat(end) * stockAmount);
-  
-  document.getElementById("demo").innerHTML = total;
-})
-}
+
